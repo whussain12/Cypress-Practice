10f0a11fdb1ee037d3b4a0843d32ed073ac4e847
Wajid Hussain <wajid.hussain@thetechmenders.com>
2024-12-13 03:10:54 +0500

    updated url

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     },
     watchForFileChanges: false,  //again 
     experimentalStudio:true, 
-  baseUrl: "http://217.76.53.78:8111", // Replace with your app's base URL ,remove comma again
+  baseUrl: "https://www.trademarkbridge.com", // Replace with your app's base URL ,remove comma again
   video: false // Disables video recording for faster CI runs (optional) another comment added
 
 
