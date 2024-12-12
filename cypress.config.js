b0116fb1035d85c699b6b1674472eb45487f8782
Wajid Hussain <wajid.hussain@thetechmenders.com>
2024-12-13 00:48:25 +0500

    add a new comment

--- cypress.config.js
+++ cypress.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
     watchForFileChanges: false,  //again 
     experimentalStudio:true, 
   baseUrl: "http://217.76.53.78:8111", // Replace with your app's base URL ,remove comma again
-  video: false // Disables video recording for faster CI runs (optional)
+  video: false // Disables video recording for faster CI runs (optional) another comment added
 
 
   },
