68a4ec09ac874235258c0a4a52081507ec623ec0
Wajid Hussain <wajid.hussain@thetechmenders.com>
2024-12-13 05:33:47 +0500

    actions and config update

--- cypress.config.js
+++ cypress.config.js
@@ -9,8 +9,10 @@ module.exports = defineConfig({
     watchForFileChanges: false,  //again 
     experimentalStudio:true, 
   baseUrl: "https://www.trademarkbridge.com", // Replace with your app's base URL ,remove comma again
-  video: false // Disables video recording for faster CI runs (optional) another comment added
-
-
+  video: false, // Disables video recording for faster CI runs (optional) another comment added
+  
   },
 });
+module.exports = defineConfig({
+  projectId: "v449vv"
+})
