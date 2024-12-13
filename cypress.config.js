1d56f3b3ea9b426fb6b1fe3d7492bbd9640d374e
Wajid Hussain <wajid.hussain@thetechmenders.com>
2024-12-13 22:51:28 +0500

    retest

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,13 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implement node event listeners here
     },
-    watchForFileChanges: true,
     experimentalStudio: true,
-    baseUrl: 'http://localhost:3000', // Replace with your application's base URL if needed
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}', // Specifies the test file pattern
-    supportFile: 'cypress/support/e2e.js', // Path to the support file
+    watchForFileChanges: false,
+    viewportWidth: 1366,
+    viewportHeight: 768,
   },
 });
