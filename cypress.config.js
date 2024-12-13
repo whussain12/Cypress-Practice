048d6109716363e804cee8c071dc94fd56c626cd
Wajid Hussain <wajid.hussain@thetechmenders.com>
2024-12-13 22:11:18 +0500

    updated

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     },
     watchForFileChanges: true,
     experimentalStudio: true,
-    // baseUrl: 'http://localhost:3000', // Replace with your application's base URL if needed
+    baseUrl: 'http://localhost:3000', // Replace with your application's base URL if needed
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}', // Specifies the test file pattern
     supportFile: 'cypress/support/e2e.js', // Path to the support file
   },
