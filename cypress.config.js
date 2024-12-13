8359861022cb1c08d6df345aec2933d177592b32
Wajid Hussain <wajid.hussain@thetechmenders.com>
2024-12-13 22:04:22 +0500

    update config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,14 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
     },
-    watchForFileChanges: false,  //again 
-    experimentalStudio:true, 
-  baseUrl: "https://www.trademarkbridge.com", // Replace with your app's base URL ,remove comma again
-  video: false, // Disables video recording for faster CI runs (optional) another comment added
-  
+    watchForFileChanges: true,
+    experimentalStudio: true,
+    // baseUrl: 'http://localhost:3000', // Replace with your application's base URL if needed
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}', // Specifies the test file pattern
+    supportFile: 'cypress/support/e2e.js', // Path to the support file
   },
 });
-module.exports = defineConfig({
-  projectId: "v449vv"
-})
