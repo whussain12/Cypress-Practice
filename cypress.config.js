475600594ffb0fd851879b80526b9d7e49836906
Wajid Hussain <wajid.hussain@thetechmenders.com>
2024-12-14 22:01:34 +0500

    added mocha report

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // Implement node event listeners here
