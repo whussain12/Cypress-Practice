78be37885b914653d4eb415f4ef711d3fb44da48
Wajid Hussain <wajid.hussain@thetechmenders.com>
2024-12-14 22:28:15 +0500

    tm contact ci cd

--- cypress/e2e/Recording 12_14_2024 at 10_05_36 PM trademark_bridge contact.cy.js
+++ cypress/e2e/Recording 12_14_2024 at 10_05_36 PM trademark_bridge contact.cy.js
@@ -4,7 +4,7 @@ describe("Recording 12/14/2024 at 10:05:36 PM trademark_bridge contact", () => {
     cy.visit("https://www.trademarkbridge.com/contact-us");
     cy.get("#navbarNavDropdown > ul > li:nth-of-type(4) > a").click();
     cy.location("href").should("eq", "https://www.trademarkbridge.com/contact-us");
-    cy.get("#MainContent_txtcn1").click().type("test lead contact recorder automation cypress locally");
+    cy.get("#MainContent_txtcn1").click().type("test lead contact recorder automation cypress ci cd");
     cy.get("#MainContent_txtem1").click();
     cy.get("#MainContent_txtem1").type("tester37@yopmail.com");
     cy.get("#MainContent_pn1").click();
